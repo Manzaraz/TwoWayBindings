6471f09c68f5ff1950fc818e5a180af60d404cfe
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-30 19:31:55 -0300

    Ch. 1.21 SwiftUI Two Way Bindings for @State using $

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount = 0
+    @State private var toggleIsOn = false
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group {
+                Spacer()
+                
+                Text("Press Count: \(pressCount)")
+                
+                Button("Press Me") {
+                    pressCount += 1
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                TextField("Enter a name", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                Text("Name entered: \(name)")
+                
+                Spacer()
+                
+                Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+                
+                Spacer()
+            }
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            ColorPicker("Pick a rectangle color: ", selection: $selectedColor)
+            
+            Spacer()
+            
+            DatePicker("Date:", selection: $selectedDate)
+            
+            Text("Selected Date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Stepper value \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
         }
+        .font(.title2)
         .padding()
     }
 }
