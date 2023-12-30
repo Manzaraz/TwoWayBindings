bee52ab434b04500d2f735d3c8de999899976c2d
Christian Manzaraz <magnusmanz@gmail.com>
2023-12-30 18:58:52 -0300

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Christian Manzaraz on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
